42a51472cacc3d833cc13f20e03ad7f9856d6fac
Christian Specht <christi@nspecht.de>
2014-10-26 01:22:34 +0200

    cache project pages from Bitbucket, first draft
    --HG--
    branch : cache

--- src/php/md-include.php
+++ src/php/md-include.php
@@ -5,24 +5,41 @@
 # Usage:
 #
 #    include_once "md-include.php";
-#    echo GetMarkdown("http://example.com/test.md");
+#    echo GetMarkdown("http://example.com/test.md", "Page Title");
 #
 
 include_once "markdown.php";
 
-function GetMarkdown($url) {
+function GetMarkdown($url, $title) {
 
-	$result = @file_get_contents($url);
+    // time (in minutes) how long a cached file is valid
+    $cachetime = 5; // 10 * 60; 
+    
+    // name of the cache file: /php/cache/Project-Name.html
+    $cachefile = __DIR__ . "/cache/" . str_replace(" ", "-", $title) . ".html";
+    echo "<!-- $cachefile -->";
+    
+    // TODO: load content from cache file if it already exists
+    
+    
+    $result = @file_get_contents($url);
 
-	if ($result === false)
-	{
-		return "Could not load $url";
-	}
-	else
-	{
-		return Markdown($result);
-	}
-	
+    if ($result === false)
+    {
+        return "Could not load $url";
+    }
+    else
+    {
+        $md = Markdown($result);
+        $md = "\r\n<!-- cached " . date('Y/m/d H:i:s') . " -->\r\n" . $md;
+        
+        $cached = fopen($cachefile, 'w');
+        fwrite($cached, $md);
+        fclose($cached);
+        
+        return $md;
+    }
+    
 }
 
 ?>
