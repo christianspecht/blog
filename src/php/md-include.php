2c20e9160d53a6ee436d45cfcf800a34718cfc15
Christian Specht <christi@nspecht.de>
2014-10-29 16:33:27 +0100

    use single quotes everywhere
    --HG--
    branch : cache

--- src/php/md-include.php
+++ src/php/md-include.php
@@ -4,11 +4,11 @@
 #
 # Usage:
 #
-#    include_once "md-include.php";
-#    echo GetMarkdown("http://example.com/test.md", "Page Title");
+#    include_once 'md-include.php';
+#    echo GetMarkdown('http://example.com/test.md', 'Page Title');
 #
 
-include_once "markdown.php";
+include_once 'markdown.php';
 
 function GetMarkdown($url, $title) {
 
@@ -16,12 +16,12 @@ function GetMarkdown($url, $title) {
     $cachetime = 10 * 60 * 60; 
     
     // name of the cache file: /php/cache/project-name.html
-    $cachefile = __DIR__ . "/cache/" . str_replace(" ", "-", strtolower($title)) . ".html";
+    $cachefile = __DIR__ . '/cache/' . str_replace(' ', '-', strtolower($title)) . '.html';
     
     // load content from cache file if it already exists
     if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
         $result = file_get_contents($cachefile);
-        $result = "\r\n<!-- cached " . date('Y/m/d H:i:s') . " -->\r\n" . $result;
+        $result .= PHP_EOL . '<!-- cached ' . date('Y/m/d H:i:s') . ' -->' . PHP_EOL;
         return $result;
     }
     
@@ -29,17 +29,17 @@ function GetMarkdown($url, $title) {
 
     if ($result === false)
     {
-        return "Could not load $url";
+        return 'Could not load ' . $url;
     }
     else
     {
-        $md = Markdown($result);
+        $html = Markdown($result);
         
         $cached = fopen($cachefile, 'w');
-        fwrite($cached, $md);
+        fwrite($cached, $html);
         fclose($cached);
         
-        return $md;
+        return $html;
     }
     
 }
