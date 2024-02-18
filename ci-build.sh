d8f638ae9ef9c4e3c999d8d7f24af6387a25d3fb
Christian Specht <christi@nspecht.de>
2024-02-18 23:52:01 +0100

    rename project pages to .php

--- ci-build.sh
+++ ci-build.sh
@@ -16,6 +16,15 @@ then
   exit 1
 fi
 
+# rename project pages
+mv ./public/bitbucket-backup/index.html ./public/bitbucket-backup/index.php
+mv ./public/missilesharp/index.html ./public/missilesharp/index.php
+mv ./public/recordset-net/index.html ./public/recordset-net/index.php
+mv ./public/roboshell-backup/index.html ./public/roboshell-backup/index.php
+mv ./public/simpleleague/index.html ./public/simpleleague/index.php
+mv ./public/tasko/index.html ./public/tasko/index.php
+mv ./public/vba-helpers/index.html ./public/vba-helpers/index.php
+
 # archive site
 mkdir build
 tar -czvf build/build.tar.gz -C public .
