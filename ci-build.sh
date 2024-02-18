b5810980d8f2b8df17c20c2a93ea3cae3c6ddf3d
Christian Specht <christi@nspecht.de>
2024-02-18 23:36:03 +0100

    delete tree commands again

--- ci-build.sh
+++ ci-build.sh
@@ -5,15 +5,11 @@ echo "[params]
 commitid = '$1'
 buildnumber = '$2'" > ./src/config-github.toml
 
-tree /home/runner/work/blog
-
 # build site
 rm -r public
 cd src
 hugo --config config.toml,config-$3.toml,config-github.toml
 cd ..
-
-tree /home/runner/work/blog
 
 if [ $? -ne 0 ]
 then
